cc1583fbe9c555ce86322d8ed5304cac94f91298
yurigurlie207 <adrienne.yao@gmail.com>
2021-03-22 11:47:06 -0400

    commit tree f2172c0b9a3705cca6cbbfcbeb14167976ad4cb1

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,30 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+    constructor() {
+      super();
+  
+      this.state = {
+        reviews: []
+      };
+    }
+  
+    componentDidMount() {
+      fetch(URL)
+        .then(res => res.json())
+        .then(response => this.setState({ reviews: response.results }));
+    }
+  
+    render() {
+      return (
+        <div className="latest-movie-reviews">
+          <h2>The Latest Reviews:</h2>
+          <MovieReviews reviews={this.state.reviews} />
+        </div>
+      );
+    }
+  }
+  
+  
+  export default LatestMovieReviewsContainer;
